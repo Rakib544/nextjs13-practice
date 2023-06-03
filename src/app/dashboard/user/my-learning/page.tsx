40c97eaca34895524d711d1c5af9f4ba7aaa258b
Rakib544 <md.rakib10122003@gmail.com>
2023-06-04 01:35:29 +0600

    error fixed

--- src/app/dashboard/user/my-learning/page.tsx
+++ src/app/dashboard/user/my-learning/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
 
-export const coursesData = [
+const coursesData = [
   {
     id: 1,
     title: "Pro Course (Content Updated up to Batch 6)",
@@ -30,34 +30,41 @@ export default function MyLearning() {
   return (
     <div>
       <div className="grid grid-cols-12 gap-6">
-        {coursesData.map((course) => (
-          <Link
-            href="/course/1/learn/lecture/1"
-            className="shadow-nav col-span-12 grid grid-cols-12 gap-3 rounded bg-white p-2 lg:col-span-6"
-            key={course.id}
-          >
-            <div className="col-span-5">
-              <Image
-                src={course.thumbnail}
-                alt={course.title}
-                height={400}
-                width={400}
-                className="h-full w-auto rounded-md"
-              />
-            </div>
-            <div className="col-span-7">
-              <h3 className="mb-1 font-bold">{course.title}</h3>
-              <p>{course.author}</p>
-              <div className="my-2 flex gap-2">
-                <div className="relative h-4 w-full rounded-full bg-slate-200">
-                  <div className="absolute left-0 top-0 block h-4 w-[95%] rounded-full bg-[#04D9FF]" />
-                </div>
-                <p>96%</p>
+        {coursesData.map(
+          (course: {
+            id: number;
+            title: string;
+            author: string;
+            thumbnail: string;
+          }) => (
+            <Link
+              href="/course/1/learn/lecture/1"
+              className="shadow-nav col-span-12 grid grid-cols-12 gap-3 rounded bg-white p-2 lg:col-span-6"
+              key={course.id}
+            >
+              <div className="col-span-5">
+                <Image
+                  src={course.thumbnail}
+                  alt={course.title}
+                  height={400}
+                  width={400}
+                  className="h-full w-auto rounded-md"
+                />
               </div>
-              <Button>Continue Courses</Button>
-            </div>
-          </Link>
-        ))}
+              <div className="col-span-7">
+                <h3 className="mb-1 font-bold">{course.title}</h3>
+                <p>{course.author}</p>
+                <div className="my-2 flex gap-2">
+                  <div className="relative h-4 w-full rounded-full bg-slate-200">
+                    <div className="absolute left-0 top-0 block h-4 w-[95%] rounded-full bg-[#04D9FF]" />
+                  </div>
+                  <p>96%</p>
+                </div>
+                <Button>Continue Courses</Button>
+              </div>
+            </Link>
+          )
+        )}
       </div>
     </div>
   );
